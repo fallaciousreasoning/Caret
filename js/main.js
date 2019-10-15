92af48307107dec0e11ebeb38b8d1fd138d6c59c
Jay Harris <jay.harris@outlook.co.nz>
2019-10-15 13:51:50 +1100

    Removes updating logic (this may need to be readded)

--- js/main.js
+++ js/main.js
@@ -36,65 +36,12 @@ require([
     command.fire("init:complete");
   });
 
-  //code to enable update checking
-  var updateID = "caret:update";
-
-  var checkUpdates = async function(isManual) {
-    var [status, details] = await chromeP.runtime.requestUpdateCheck();
-    if (status == "update_available") {
-      chrome.runtime.onUpdateAvailable.addListener(async function() {
-      await chromeP.notifications.clear(updateID);
-      updateID = await chromeP.notifications.create(updateID, {
-          type: "basic",
-          iconUrl: "icon-128.png",
-          title: i18n.get("notificationUpdateAvailable"),
-          message: i18n.get("notificationUpdateDetail", details.version),
-          buttons: [
-            { title: i18n.get("notificationUpdateOK") },
-            { title: i18n.get("notificationUpdateWait") }
-          ]
-        });
-      });
-    } else {
-      if (isManual) {
-        updateID = await chromeP.notifications.create(updateID, {
-        type: "basic",
-        iconUrl: "icon-128.png",
-        title: i18n.get("notificationNoUpdateTitle"),
-        message: i18n.get("notificationNoUpdateDetail")
-        });
-      }
-    }
-  };
-
-  // manage updates on start
-  Settings.pull("user").then(async function(cfg) {
-    if (cfg.user.promptForUpdates !== false) checkUpdates();
-    if (cfg.user.updateNotifications == "launch") {
-      var background = await chromeP.runtime.getBackgroundPage();
-      var manifest = chrome.runtime.getManifest();
-      // console.log(background.updateVersion, manifest.version);
-      if (background.updateVersion && background.updateVersion != manifest.version) {
-        background.showUpdateNotification();
-        background.updateVersion = null;
-      }
-    }
-  });
-  command.on("app:check-for-updates", checkUpdates);
-
   //export update notification preference, possibly others
   command.on("init:restart", async function() {
     var cfg = await Settings.pull("user");
     chromeP.storage.sync.set({
       updateNotifications: cfg.user.updateNotifications
     });
-  });
-
-  chrome.notifications.onButtonClicked.addListener(function(id, index) {
-    if (id != updateID) return;
-    if (index == 0) {
-      chrome.runtime.reload();
-    }
   });
 
   command.on("app:exit", async function() {
